526e73032fce230a864a5a8944fee6c18a396ff8
Jhonathan <guarino90@gmail.com>
2023-02-01 12:01:39 -0300

    add border to massage-images and arrow-icon

---
+++ javascript/main-arrow.js
@@ -0,0 +1,15 @@
+/*const arrrowUp = document.querySelector(".arrow-up");
+const arrrowDown = document.querySelector(".arrow-down");
+const messageWrpp = document.querySelector(".message-wrapper");
+
+arrrowUp.addEventListener("click", function () {
+  messageWrpp.style = "display:flex;";
+  arrrowUp.style = "opacity:0;";
+  arrrowDown.style = "opacity:1 ;";
+});
+
+arrrowDown.addEventListener("click", function () {
+  messageWrpp.style = "display:none;";
+  arrrowUp.style = "opacity:1;";
+  arrrowDown.style = "opacity:0 ;";
+});
